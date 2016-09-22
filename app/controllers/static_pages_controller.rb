e969dbe008f81cfea5725e9a04416a348b332c62
buncis mamen <buncismamen@gmail.com>
2016-09-22 23:31:33 +0700

    bootstrap header dan link_to

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticPagesController < ApplicationController
+  
+  def home
+  end
+
+  def informasi
+  end
+
+  def data
+  end
+
+  def ramalan
+  end
+
+  def info
+  end
+
+  def about
+  end
+end
